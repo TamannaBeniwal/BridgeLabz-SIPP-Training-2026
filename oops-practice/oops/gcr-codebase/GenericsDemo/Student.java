40d8c552843df09b892177090112b88aae61eeed
tamanna <tamannabeniwal007@gmail.com>
2026-07-08 09:50:52 +0530

    [Tamanna] add:Generics Demo Problems

--- oops-practice/oops/gcr-codebase/GenericsDemo/Student.java
+++ oops-practice/oops/gcr-codebase/GenericsDemo/Student.java
@@ -1,15 +1,15 @@
-public class Student {
-
-    private int id;
-    private String name;
-
-    public Student(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return id + " " + name;
-    }
-}
+//public class Student {
+//
+//    private int id;
+//    private String name;
+//
+//    public Student(int id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return id + " " + name;
+//    }
+//}
